67987e7dd2629ed24472834694fe006e96cbc7d2
anassuv <114801727+anassuv@users.noreply.github.com>
2022-12-03 08:50:57 +0300

    all added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 345, 897, 568, 234 };
+            int k = 0;
+
+            Console.Write("\n[");
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i < a.Length - 1)
+                    Console.Write("{0}, ", a[i]);
+                else
+                    Console.Write("{0}", a[i]);
+                if (a[i] % 2 == 0) k++;
+            }
+            Console.WriteLine("] -> {0}", k);            
+
+            Console.ReadKey();
+        }
+    }
+}
